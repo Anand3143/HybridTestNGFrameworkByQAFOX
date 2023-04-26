dab1dd5f9c917dd03d384229b0ed0829a607a64a
Anand3143 <anand.rgukt@gmail.com>
2023-04-26 07:29:50 +0530

    added more details to search test class

--- src/test/java/com/tutorialsninja/qa/testcases/SearchTest.java
+++ src/test/java/com/tutorialsninja/qa/testcases/SearchTest.java
@@ -9,7 +9,7 @@ import org.testng.annotations.Test;
 import com.tutorialsninja.qa.base.Base;
 import com.tutorialsninja.qa.pages.SearchPage;
 
-//Comment Updated - added more details
+//Comment Updated - added more details in the SearchTest class
 
 public class SearchTest extends Base {
 	
